7d6dba2ac95dfe49d7c04de5101e3cad72458097
Manoj <manojgowdacs35@gmail.com>
2023-11-03 17:59:29 +0530

    deleting note featured

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,10 @@ export default function Sidebar(props){
             onClick={() => props.setCurrentNoteId(note.id)}
           >
             <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+            <button className = 'delete-btn'
+            onClick={(event) => props.deleteNote(event, note.id)}>
+                <i className="gg-trash trash-icon"></i>
+            </button>
           </div>
         </div>
       ));
@@ -35,4 +39,5 @@ Sidebar.propTypes = {
     }),
     setCurrentNoteId : PropTypes.func.isRequired, 
     newNote: PropTypes.func.isRequired,
+    deleteNote: PropTypes.func.isRequired,
 };
